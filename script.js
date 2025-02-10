40d316ce2b2cfbd6c380110d0e8b27ec1abbe462
ツTchoa <miguelthomasochoa@gmail.com>
2025-02-10 11:10:44 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  /* if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }*/
+
+  html.classList.toggle("light")
+  // para mudar a foto ao apertar o botao - 
+ 
+  // pegar a tag img
+  
+  // substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  //se tiver sem light mode, manter a imagem normal
+}
